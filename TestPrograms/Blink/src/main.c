4f579a4fe0efe4ffc91a26da108412df387669c9
Florian Baumgartner <florian.b-h@bluewin.ch>
2021-10-08 14:16:49 +0200

    Updated Blink frequency

--- TestPrograms/Blink/src/main.c
+++ TestPrograms/Blink/src/main.c
@@ -16,9 +16,9 @@ void app_main() {
     // Main loop
     while(true) {
         gpio_set_level(LED, 0);
-        vTaskDelay(500 / portTICK_RATE_MS);
+        vTaskDelay(100 / portTICK_RATE_MS);
         gpio_set_level(LED, 1);
-        vTaskDelay(500 / portTICK_RATE_MS);
+        vTaskDelay(100 / portTICK_RATE_MS);
     }
 
 }

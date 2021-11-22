8398cd6249f96ec3fa23959c0a4ea6441bc88e3e
Luca Jost <47953330+l-jost@users.noreply.github.com>
2021-11-22 16:47:42 +0100

    Formatted

--- Firmware/FleetMonitor_V0.1/src/task_frame_handle.cpp
+++ Firmware/FleetMonitor_V0.1/src/task_frame_handle.cpp
@@ -20,49 +20,67 @@ extern USBCDC USBSerial;
 
 DynamicJsonDocument doc(DOCUMENT_SIZE);
 
+typedef enum{
+  FILE_UNDEFINED,
+  FILE_CONFIG,
+  FILE_SYSTEM,
+} file_type_t;
+
 
 bool send_data_to_client();
 
-void task_frame_handler(void *pvParameter) {
+void task_frame_handler(void *pvParameter){
   vTaskDelay(1000);
   while(1){
       Fms frame;
       if( xQueueReceive( fmsQueue, &(frame),portMAX_DELAY ) == pdPASS ){
-            StaticJsonDocument<256> entry;
-            entry["ts"] = xTaskGetTickCount();
-            char pgn[5];
-            snprintf(pgn, 5, "%0X",frame.getPgn());
-            pgn[4] = 0;
-            entry["pgn"] = pgn;
-            char dataBuffer[17];
-            for(int i = 0; i < 8; i++){
-                sprintf(dataBuffer+(2*i), "%02X", frame.getData()[i]);
-            }
-            dataBuffer[16] = 0;
-            entry["data"]=dataBuffer;
-            doc.add(entry);
-            entry.clear();
-            USBSerial.println(doc.size());
-            if(doc.size() >= NR_OF_FRAMES){
-              if(network_connected){
-                send_data_to_client();
-                serializeJson(doc, USBSerial);
-              }
-              doc.clear();
-            }            
+        StaticJsonDocument<256> entry;
+        entry["ts"] = xTaskGetTickCount();
+        char pgn[5];
+        snprintf(pgn, 5, "%0X",frame.getPgn());
+        pgn[4] = 0;
+        entry["pgn"] = pgn;
+        char dataBuffer[17];
+        for(int i = 0; i < 8; i++){
+          sprintf(dataBuffer+(2*i), "%02X", frame.getData()[i]);
+        }
+        dataBuffer[16] = 0;
+        entry["data"]=dataBuffer;
+        doc.add(entry);
+        entry.clear();
+        //USBSerial.println(doc.size());
+        if(doc.size() >= NR_OF_FRAMES){
+          if(network_connected){
+            USBSerial.println("Data sent!");
+            send_data_to_client();
+          }
+          doc.clear();
+        }            
       }
   }
 }
 
-bool send_data_to_client() {
-
+bool send_data_to_client(){
+  int status;
   String contentType = "application/x-www-form-urlencoded";
   String postData;
   serializeJson(doc, postData);
-  int httpResponseCode = client.post("/", contentType, postData);
+  status = client.post("/", contentType, postData);
+
+  int statusCode = client.responseStatusCode();§
+  //String response = client.responseBody();
+
+  USBSerial.print("Status code: ");
+  USBSerial.println(statusCode);
+  //Serial.print("Response: ");
+  //Serial.println(response);
 
   USBSerial.print("Response Code: ");
-  USBSerial.println(httpResponseCode);
+  USBSerial.println(status);
   return true;
+}
+
+bool get_file_from_server(){
+
 }
 
